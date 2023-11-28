c4970c0910b0c26ed11d01c5fe6b48b9000a1296
Sowndaryakp <100479632+Sowndaryakp@users.noreply.github.com>
2023-11-28 21:24:04 +0530

    Update pattern7.js

--- JavaScriptFundamentals2.1to2.18/Assignments/Assignment3/pattern7.js
+++ JavaScriptFundamentals2.1to2.18/Assignments/Assignment3/pattern7.js
@@ -1,5 +1,65 @@
+// A
+// BC
+// DEF
 
-test();
-function test(){
-    console.log("something");
+for (let z = 1; z <= 3; z++) {
+    let charCode = 65; 
+    for (let i = 1; i <= z; i++) {
+        process.stdout.write(String.fromCharCode(charCode) + " ");
+        charCode++;
+    }
+    console.log("");
 }
+
+// A
+// BB
+// CCC
+for (let z = 1; z <= 3; z++) {
+    let charCode = 65 + z - 1; 
+    for (let i = 1; i <= z; i++) {
+        process.stdout.write(String.fromCharCode(charCode));
+    }
+    console.log("");
+}
+
+// A
+// AB
+// ABC
+
+for (let z = 1; z <= 3; z++) {
+    let charCode = 65; 
+    for (let i = 1; i <= z; i++) {
+        process.stdout.write(String.fromCharCode(charCode));
+        charCode++;
+    }
+    console.log("");
+}
+
+// A B C D E
+// B C D E
+// C D E
+// D E
+// E
+
+for (let i = 0; i < 5; i++) {
+    let charCode = 65 + i; // ASCII code for 'A' plus the current iteration
+    for (let j = i; j < 5; j++) {
+        process.stdout.write(String.fromCharCode(charCode) + " ");
+        charCode++;
+    }
+    console.log("");
+}
+
+// A A A A A
+// B B B B
+// C C C
+// D D
+// E
+for (let i = 0; i < 5; i++) {
+    let charCode = 65 + i; // ASCII code for 'A' plus the current iteration
+    for (let j = 0; j < 5 - i; j++) {
+        process.stdout.write(String.fromCharCode(charCode) + " ");
+    }
+    console.log("");
+}
+
